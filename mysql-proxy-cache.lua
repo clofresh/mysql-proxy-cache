6f21baa7557665cfa22c1e072ac383268e9dd260
Daniel Lockard <danlock2@daniel-lockards-macbook.local>
2009-07-08 16:00:37 -0500

    Modified mysql-proxy-cache.lua so that it works again. Added a timeout for the cache.

--- mysql-proxy-cache.lua
+++ mysql-proxy-cache.lua
@@ -1,11 +1,10 @@
 require('luarocks.require')
 require('md5')
 require('Memcached')
-
 local memcache = Memcached.Connect()
 cache_hits = 0
 cache_misses = 0
-
+cache_timeout = 30
 function is_query(packet)
     return packet:byte() == proxy.COM_QUERY
 end
@@ -21,29 +20,31 @@ end
 function cache_get(query)
     local result = deserialize(memcache:get(to_hash(query)))
     if result then
-        print('HIT: '..to_hash(query)..' ('..query..')')
+--        print('HIT: '..to_hash(query)..' ('..query..')')
         cache_hits = cache_hits + 1
     else
-        print('MISS: '..to_hash(query)..' ('..query..')')
+--      print('MISS: '..to_hash(query)..' ('..query..')')
         cache_misses = cache_misses + 1
     end
-    
-    print('Cache hit ratio: '..cache_hits..'/'..cache_misses..' = '..cache_hits/cache_misses)
-    
+
+--    print('Cache hit ratio: '..cache_hits..'/'..cache_misses..' = '..cache_hits/cache_misses)
+
     return result
 end
 
 function cache_set(result_packet)
+    local resultset_is_needed = false
     local query = result_packet.query:sub(2)
     local field_count = 1
-    local fields = result_packet.resultset.fields 
+    local fields = result_packet.resultset.fields
     local resultset = {rows={}, fields={}}
-    
-    print('SET: '..to_hash(query)..' ('..query..')')
-  
+
+  --  print('SET: '..to_hash(query)..' ('..query..')')
+
     while fields[field_count] do
         local field = fields[field_count]
-        table.insert(resultset.fields, {type=field.type, name=field.name})
+	--added third option, expiry time.
+        table.insert(resultset.fields, {type=field.type, name=field.name} )
         field_count = field_count + 1
     end
 
@@ -51,7 +52,7 @@ function cache_set(result_packet)
         table.insert(resultset.rows, row)
     end
 
-    memcache:set(to_hash(query), serialize(resultset))
+    memcache:set(to_hash(query), serialize(resultset), cache_timeout)
 end
 
 function serialize(o)
@@ -67,7 +68,7 @@ function serialize(o)
     elseif o_type == "table" then
         table.insert(result, "{")
         for key, value in pairs(o) do
-            for i, str in pairs({"[", serialize(key), "]=", 
+            for i, str in pairs({"[", serialize(key), "]=",
                                 serialize(value), ","}) do
                 table.insert(result, str)
             end
@@ -92,9 +93,6 @@ function deserialize(s)
     end
 end
 
-
-
-
 function read_query( packet )
     if is_query(packet) then
         local query = packet:sub(2)
@@ -105,18 +103,17 @@ function read_query( packet )
                 -- Cache hit
                 proxy.response.type = proxy.MYSQLD_PACKET_OK
                 proxy.response.resultset = resultset
-                
+
                 return proxy.PROXY_SEND_RESULT
             else
                 -- Cache miss
-                proxy.queries:append(1, packet)
-                
+                proxy.queries:append(1, packet,{resultset_is_needed = true})
+
                 return proxy.PROXY_SEND_QUERY
             end
         end
     end
 end
-
 
 function read_query_result(result_packet)
     -- This only gets called if the proxy.queries queue is modified
