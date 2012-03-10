bc82a3f9adbb9cdd82c011d50363caae883d83ae
Rob Smith <kormoc@gmail.com>
2012-03-10 02:25:18 -0800

    A lot of updates. Allow embedded admin commands in the query string as comments. Allow flushing of queries dynamically. Change the hash function to reduce collisions. Use the working database in the hash to prevent mismatched cache positive hits. use sha1 to hash.

--- mysql-proxy-cache.lua
+++ mysql-proxy-cache.lua
@@ -1,45 +1,74 @@
 require('luarocks.require')
 require('md5')
+require('sha1')
 require('Memcached')
-local memcache = Memcached.Connect()
+
 cache_hits = 0
 cache_misses = 0
-cache_timeout = 30
+
+-- Change this to your memcached server(s)
+local memcache = Memcached.Connect('127.0.0.1', 11211)
+
 function is_query(packet)
     return packet:byte() == proxy.COM_QUERY
 end
 
-function is_cacheable(query)
-    return query:sub(1,6):lower() == 'select'
+function get_cache_ttl(query)
+    -- Are we instructed to cache this query?
+    local ttl = string.match(query, '/%* CACHE: %d+ %*/')
+    if not ttl
+    then
+        return nil
+    end
+    
+    ttl = string.match( ttl, '%d+')
+    return ttl
+end
+
+function HexDumpString(str,spacer)
+    return (
+        string.gsub(str,"(.)",
+            function (c)
+                return string.format("%02X%s",string.byte(c), spacer or "")
+            end)
+        )
 end
 
 function to_hash(query)
-    return md5.sumhexa(query)
+    local hash = proxy.connection.client.default_db..'-'
+    -- Remove comments from the query to allow different commands
+    -- to hash to the same value
+    query = (string.gsub(query, '/%* .- %*/', ''))
+    -- Trim whitespace
+    query = (string.gsub(query, "^%s*(.-)%s*$", "%1"))
+    
+    hash = hash..string.len(query)..'-'..sha1(query)..'-'..HexDumpString(query)
+    hash = string.sub(hash, 1, 250)
+    
+    return hash
 end
 
 function cache_get(query)
     local result = deserialize(memcache:get(to_hash(query)))
-    if result then
-        print('HIT: '..to_hash(query)..' ('..query..')')
-        cache_hits = cache_hits + 1
-    else
-        print('MISS: '..to_hash(query)..' ('..query..')')
-        cache_misses = cache_misses + 1
-    end
-
-    print('Cache hit ratio: '..cache_hits..'/'..cache_misses..' = '..cache_hits/cache_misses)
+--    if result then
+--        print('HIT: '..to_hash(query)..' ('..query..')')
+--        cache_hits = cache_hits + 1
+--    else
+--        print('MISS: '..to_hash(query)..' ('..query..')')
+--        cache_misses = cache_misses + 1
+--    end
+--
+--    print('Cache hit ratio: '..cache_hits..'/'..cache_misses..' = '..cache_hits/cache_misses)
 
     return result
 end
 
 function cache_set(result_packet)
-    local resultset_is_needed = false
     local query = result_packet.query:sub(2)
+    local ttl = get_cache_ttl(query)
     local field_count = 1
     local fields = result_packet.resultset.fields
     local resultset = {rows={}, fields={}}
-
-    print('SET: '..to_hash(query)..' ('..query..')')
 
     while fields[field_count] do
         local field = fields[field_count]
@@ -52,7 +81,7 @@ function cache_set(result_packet)
         table.insert(resultset.rows, row)
     end
 
-    memcache:set(to_hash(query), serialize(resultset), cache_timeout)
+    memcache:set(to_hash(query), serialize(resultset), ttl)
 end
 
 function serialize(o)
@@ -96,8 +125,23 @@ end
 function read_query( packet )
     if is_query(packet) then
         local query = packet:sub(2)
+        local ttl = get_cache_ttl(query)
+        
+        -- Expire the cache if we are instructed to
+        if string.match(query, '/%* FLUSH %*/')
+            then
+            memcache:delete(to_hash(query))
+            proxy.response.type = proxy.MYSQLD_PACKET_OK
+            proxy.response.resultset = {
+                fields = {
+                        { type = proxy.MYSQL_TYPE_LONG, name = "FLUSH", },
+                },
+                rows = { { 'Successful' } }
+            }
+            return proxy.PROXY_SEND_RESULT
+        end
 
-        if is_cacheable(query) then
+        if ttl then
             local resultset = cache_get(query)
             if resultset then
                 -- Cache hit
@@ -111,7 +155,7 @@ function read_query( packet )
 
                 return proxy.PROXY_SEND_QUERY
             end
-        end
+    end
     end
 end
 
